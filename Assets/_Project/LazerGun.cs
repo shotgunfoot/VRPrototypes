d82d1fc27d7ebaae3f4451ceb98dba44ab015839
shotgunfoot <sphillips93@outlook.com>
2018-12-18 17:56:16 +0000

    Initial commit

---
+++ Assets/_Project/LazerGun.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Valve.VR.InteractionSystem
+{
+    public class LazerGun : MonoBehaviour
+    {
+        //The point where the gun is held in hand.
+        public Transform HandleTransform;
+
+        private void Start() {
+            transform.localRotation = Quaternion.Euler(4.826f, -85.775f, -62.35f);
+            
+        }
+        
+    }
+}
